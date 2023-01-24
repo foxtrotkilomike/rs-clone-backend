013d496fc2d3989331a2de99e4686037d70f494b
Philipp Khromov <filooloog@mail.ru>
2023-01-24 12:30:07 +0300

    refactor: move app.listen to mongoose.connect callback

--- index.js
+++ index.js
@@ -8,17 +8,17 @@ const successMessages = require('./general/successMessages');
 const mongoDB = MONGO_CONNECT_QUERY;
 mongoose
   .connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log(successMessages.server.dbConnected))
+  .then(() => {
+    console.log(successMessages.server.dbConnected);
+    app.listen(PORT, (error) => {
+      if (error) {
+        console.error(error);
+      } else {
+        console.log(successMessages.server.listen(PORT));
+      }
+    });
+  })
   .catch((error) => console.error(error));
 
 const { connection } = mongoose;
-
 connection.on('error', console.error.bind(console, errorMessages.general.mongoConnection));
-
-app.listen(PORT, (error) => {
-  if (error) {
-    console.error(error);
-  } else {
-    console.log(successMessages.server.listen(PORT));
-  }
-});
